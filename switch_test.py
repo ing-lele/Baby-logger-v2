d308c0cb5c3c3c9726e447c1153dbc68d4a7ad10
inglele <inglele@live.com>
2022-07-02 11:00:38 -0700

    button script test updated

--- switch_test.py
+++ switch_test.py
@@ -1,9 +1,64 @@
+print('Import library')
 import RPi.GPIO as GPIO          #Import GPIO library
 import time                      #Import time library
-GPIO.setmode(GPIO.BOARD)         #Set GPIO pin numbering
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Enable input and pull up resistors
+
+#----------------------------------------------------------
+#CONFIGURATION SETTINGS
+print('Config Settings')
+
+pee_led_pin = 12        #Green LED
+#             34        #GND
+fed_led_pin = 16        #Blue LED
+poo_led_pin = 20        #Red LED
+
+pee_switch_pin = 2     #Green Switch
+fed_switch_pin = 3     #Blue Switch
+poo_switch_pin = 4     #Red Switch
+
+
+#----------------------------------------------------------
+#SETUP GPIO
+print('setup GPIO')
+
+#GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+# Setup Switch - PullUp configuration
+# Switch OFF = +3.3V  
+# Switch ON = GND
+# Info https://electrosome.com/using-switch-raspberry-pi/
+GPIO.setup(pee_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(fed_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(poo_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#Setup LED
+GPIO.setup(pee_led_pin, GPIO.OUT)
+GPIO.setup(fed_led_pin, GPIO.OUT)
+GPIO.setup(poo_led_pin, GPIO.OUT)
+
+#----------------------------------------------------------
+# Test Switch -> LED
+print('Test Switch')
+
 while True:
-    input_state = GPIO.input(12) #Read and store value of input to a variable
-    if input_state == False:     #Check whether pin is grounded
-       print('Button Pressed')   #Print 'Button Pressed'
-       time.sleep(0.3)           #Delay of 0.3s
+    input_state_pee = GPIO.input(pee_switch_pin) #PEE
+    input_state_fed = GPIO.input(fed_switch_pin) #FED
+    input_state_poo = GPIO.input(poo_switch_pin) #POO
+
+    curr_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    curr_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+    if (input_state_pee == GPIO.LOW):
+        print('PEE Switch - curr_date @ curr_time')     #Print 'PEE Switch'
+        GPIO.output(pee_led_pin, GPIO.LOW)        
+
+    elif (input_state_fed == GPIO.LOW):
+        print('FED Switch - curr_date @ curr_time')     #Print 'FED Switch'
+        GPIO.output(fed_led_pin, GPIO.LOW)
+
+    elif (input_state_poo == GPIO.LOW):
+        print('POO Switch - curr_date @ curr_time')     #Print 'POO Switch'
+        GPIO.output(poo_led_pin, GPIO.LOW)
+
+    time.sleep(0.3)           #Delay of 1s
+
+
+GPIO.cleanup()
