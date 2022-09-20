d9419477bae5cf03e95631d1cc912a99b3395823
inglele <inglele@live.com>
2022-09-20 16:08:40 -0700

    updated swtich and LED case logic

--- switch_test.py
+++ switch_test.py
@@ -4,18 +4,19 @@ import time                      #Import time library
 import datetime
 
 #----------------------------------------------------------
-#CONFIGURATION SETTINGS
+#CONFIGURATION SETTINGS (via GPIO number)
 print('Config Settings')
 
 pee_led_pin = 12        #Green LED
-#             34        #GND
+#         PIN#34        #GND
 fed_led_pin = 16        #Blue LED
 poo_led_pin = 20        #Red LED
 
-pee_switch_pin = 2     #Green Switch
-fed_switch_pin = 3     #Blue Switch
-poo_switch_pin = 4     #Red Switch
-
+#            PIN#1      VCC +3.3V
+pee_switch_pin = 2      #Green Switch
+fed_switch_pin = 3      #Blue Switch
+poo_switch_pin = 4      #Red Switch
+#            PIN#9      GND
 
 #----------------------------------------------------------
 #SETUP GPIO
@@ -23,14 +24,17 @@ print('setup GPIO')
 
 GPIO.cleanup()  #cleanup any previous config 
 #GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+
+GPIO.setmode(GPIO.BCM) #setup via GPIO pin number
+
 # Setup Switch - PullDown configuration
 # Switch Closed = +3.3V (VCC)
-# Switch Open = Float (GND via PullDown)
+# Switch Open = 0V (GND via PullDown)
 # Info https://electrosome.com/using-switch-raspberry-pi/
 GPIO.setup(pee_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 GPIO.setup(fed_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(poo_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 #Setup LED
 GPIO.setup(pee_led_pin, GPIO.OUT)
 GPIO.setup(fed_led_pin, GPIO.OUT)
@@ -38,23 +42,29 @@ GPIO.setup(poo_led_pin, GPIO.OUT)
 
 #----------------------------------------------------------
 # Test LED
+print('Test LED')
 
-# clean up
+# reset LED
 GPIO.output(pee_led_pin, GPIO.LOW)
 GPIO.output(fed_led_pin, GPIO.LOW)
 GPIO.output(poo_led_pin, GPIO.LOW)
 
-# test on
+# test PEE
+print('Test RED')
 GPIO.output(pee_led_pin, GPIO.HIGH)
-time.sleep(3)
-GPIO.output(fed_led_pin, GPIO.HIGH)
-time.sleep(3)
-GPIO.output(poo_led_pin, GPIO.HIGH)
-time.sleep(3)
-
-# clean up
+time.sleep(5)
 GPIO.output(pee_led_pin, GPIO.LOW)
+
+# test FED
+print('Test BLUE')
+GPIO.output(fed_led_pin, GPIO.HIGH)
+time.sleep(5)
 GPIO.output(fed_led_pin, GPIO.LOW)
+
+# test POO
+print('Test GREEN')
+GPIO.output(poo_led_pin, GPIO.HIGH)
+time.sleep(5)
 GPIO.output(poo_led_pin, GPIO.LOW)
 
 #----------------------------------------------------------
@@ -62,32 +72,46 @@ GPIO.output(poo_led_pin, GPIO.LOW)
 print('Test Switch')
 
 while True:
+    # Print current status
     print('Pee switch: ',GPIO.input(pee_switch_pin))
     print('Fed switch: ',GPIO.input(fed_switch_pin))
     print('Poo switch: ',GPIO.input(poo_switch_pin))
     
+    # read value
     input_state_pee = GPIO.input(pee_switch_pin) #PEE
     input_state_fed = GPIO.input(fed_switch_pin) #FED
     input_state_poo = GPIO.input(poo_switch_pin) #POO
 
+    # Set time + date
     curr_date = datetime.datetime.now().strftime("%Y-%m-%d")
     curr_time = datetime.datetime.now().strftime("%H:%M:%S")
 
+    # Active switch
     if (input_state_pee == GPIO.LOW):
-        print('PEE Switch - ', curr_date , curr_time)     #Print 'PEE Switch'
-        GPIO.output(pee_led_pin, GPIO.HIGH)        
+        print('PEE Active - ', curr_date , curr_time)     #Print 'PEE Active'
+        GPIO.output(pee_led_pin, GPIO.HIGH)
 
-    elif (input_state_fed == GPIO.LOW):
-        print('FED Switch - ', curr_date, curr_time)     #Print 'FED Switch'
+    if (input_state_fed == GPIO.LOW):
+        print('FED Active - ', curr_date, curr_time)     #Print 'FED Active'
         GPIO.output(fed_led_pin, GPIO.HIGH)
 
-    elif (input_state_poo == GPIO.LOW):
-        print('POO Switch - ', curr_date, curr_time)     #Print 'POO Switch'
+    if (input_state_poo == GPIO.LOW):
+        print('POO Active - ', curr_date, curr_time)     #Print 'POO Active'
         GPIO.output(poo_led_pin, GPIO.HIGH)
+    
+    # Clear switch
+    if (input_state_pee == GPIO.HIGH):
+        print('PEE Clear - ', curr_date , curr_time)     #Print 'PEE Clear'
+        GPIO.output(pee_led_pin, GPIO.LOW)
 
-    time.sleep(0.3)           #Delay of 1s
+    if (input_state_fed == GPIO.HIGH):
+        print('FED Clear - ', curr_date, curr_time)     #Print 'FED Clear'
+        GPIO.output(fed_led_pin, GPIO.LOW)
 
+    if (input_state_poo == GPIO.HIGH):
+        print('POO Clear - ', curr_date, curr_time)     #Print 'POO Clear'
+        GPIO.output(poo_led_pin, GPIO.LOW)
 
-
+    time.sleep(5)           #Delay of 5s
 
 GPIO.cleanup()
