4f68a06a361237c7429c466e6c4d81c212b56a7c
inglele <inglele@live.com>
2022-09-22 18:54:32 -0700

    Create new SQLDB variable file

---
+++ mysql_variables.py
@@ -0,0 +1,10 @@
+### IngLele Fork at 26 Jun 2022 from https://github.com/tommygober/Baby-logger
+# =========================================================
+# Changes: 
+# * Login information to MySQL DB
+# =========================================================
+
+db_host = "localhost"
+db_user = "logger"
+db_pass = "password"    # update to correct password
+db_name = "babylogger"
