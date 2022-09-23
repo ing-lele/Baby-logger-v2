915bce189ca21050e5eb23139f7354c83383693a
inglele <inglele@live.com>
2022-09-23 14:06:37 -0700

    Updated MySQL PHP page

---
+++ website/mysql_variables.php
@@ -0,0 +1,11 @@
+// IngLele Fork at 26 Jun 2022 from https://github.com/tommygober/Baby-logger
+// =========================================================
+// Changes: 
+// * Login information to MySQL DB
+// =========================================================
+// Settings
+// host, user, and password settings
+$db_host = "localhost";
+$db_user = "logger";
+$db_pass = "password";      // update to correct password
+$db_name = "babylogger";
