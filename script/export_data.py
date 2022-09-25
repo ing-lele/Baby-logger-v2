86de6206111ae79f88629724a61fe6ce83d55e4f
inglele <inglele@live.com>
2022-09-25 13:11:27 -0700

    moved to open/write functions

--- script/export_data.py
+++ script/export_data.py
@@ -9,7 +9,6 @@
 from turtle import end_fill
 import os
 import sys
-import csv
 import time
 import datetime
 import pymysql
@@ -27,68 +26,80 @@ debug_on = 1            #DEBUG - Enable debug print
 # MySQLdb.db_name 
 
 #----------------------------------------------------------
-#Setup DB
-if(debug_on): print("DEBUG - DB Connection settings:", mysql_variables.db_host, mysql_variables.db_user, mysql_variables.db_pass, mysql_variables.db_name)     # DEBUG - Print DB info
-
-try:
-    db = MySQLdb.connect(host=mysql_variables.db_host, user=mysql_variables.db_user, password=mysql_variables.db_pass, database=mysql_variables.db_name)
-    curs = db.cursor()
-except MySQLdb.Error as er:
-        print("ERROR - Error connecting to MariaDB Platform: {e}")
-        sys.exit(1)
-
-#---------------------------------------------------------
-# FUNCTION: Write to DB
-# Table structure: CREATE TABLE buttondata(
-#	id INT PRIMARY KEY auto_increment,
-#	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+# FUNCTION: Read table from DB
+#
+# Table structure: buttondata(
+#	id INT PRIMARY,
+#	created TIMESTAMP,
 #	category TEXT,
 #	state TEXT); 
 #
 #---------------------------------------------------------
-def write_sql_to_file(file_name, sql, with_header=True, delimiter=',',quotechar='"',quoting=csv.QUOTE_NONNUMERIC, con_sscursor=False):
-    
-    if(debug_on): print("DEBUG - Write to file function")
-
-    cur = db.cursor(pymysql.cursors.SSCursor) if con_sscursor else db.cursor()
-    cur.execute(sql)
-    header = [field[0] for field in cur.description]
-    
-    # Open function - https://docs.python.org/3/library/functions.html#open
-    ofile = open(file_name,'ab')                 # Open file for Write (and create if needed) + Binary mode
+def fetch_table_data(table_name):
+    #Connect to DB
+    try:
+        db = MySQLdb.connect(host=mysql_variables.db_host, user=mysql_variables.db_user, password=mysql_variables.db_pass, database=mysql_variables.db_name)
+        curs = db.cursor()
+    except MySQLdb.Error as er:
+        print("ERROR - Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
 
     try:
-        # CSV functions - https://docs.python.org/3/library/csv.html
-        csv_writer = csv.writer(ofile, delimiter=delimiter, quotechar=quotechar,quoting=quoting)
+        # Set and execute query
+        sql = "SELECT * FROM " + table_name + " ORDER BY id DESC"
+        if(debug_on): print("DEBUG - ", sql)
+        curs.execute(sql)
 
-        if with_header:
-            csv_writer.writerow(header)
-        if con_sscursor:
-            while True:
-                x = cur.fetchone()
-                if x:
-                    csv_writer.writerow(x)
-                else:
-                    break
-        else:
-            for x in cur.fetchall():
-                csv_writer.writerow(x)
+        # Get header
+        header = [row[0] for row in curs.description]
+
+        # Get data
+        rows = curs.fetchall()
     finally:
-        cur.close()     # close cursor
-        ofile.close()   # close file
+        # Closing connection
+        db.close()
+
+    return header, rows
+    
+#---------------------------------------------------------
+# FUNCTION: Write table data to CSV file
+#
+#---------------------------------------------------------
+def export_file(table_name, file_name):
+    
+    if(debug_on): print("DEBUG - Write to file function")
+    
+    # Read SQL data
+    header, rows = fetch_table_data(table_name)
+
+    try:
+        # Create CSV file
+        ofile = open(file_name,'w')
+       
+        # Write header
+        ofile.write(','.join(header) + '\n')
+
+        # Write rows
+        for row in rows:
+            ofile.write(','.join(str(r) for r in row) + '\n')
+
+    finally:
+        # Close file
+        ofile.close()
+
+    if(debug_on): print(str(len(rows)) + ' rows written successfully to ' + ofile.name)
 
 #---------------------------------------------------------
 
-# Set filename, SQL query
-file_name = "~/Baby-logger/" + "button-data_" + datetime.datetime.now().strftime("%Y-%m-%d") + ".csv";
-sql = "SELECT * FROM buttondata ORDER BY id DESC"
+# Set table and file name
+table_name = "button-data"
+file_name = table_name + "_" + datetime.datetime.now().strftime("%Y-%m-%d") + ".csv";
 
-if(debug_on): print("DEBUG - File:", file_name, "SQL function:", sql)
+if(debug_on): print("DEBUG - File:", file_name)
 
 # Call write function
-write_sql_to_file(file_name,sql)
+export_file(table_name, file_name)
 
 time.sleep(1)
 
-# Close DB
-db.close()
+
