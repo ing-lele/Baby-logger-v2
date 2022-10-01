4e34e407cd5f9c98a11146ba956c7bb9925c0954
inglele <inglele@live.com>
2022-10-01 12:14:31 -0700

    added more debug log in off.php

--- website/off.php
+++ website/off.php
@@ -12,10 +12,21 @@
         <?php 
             //Execute Shutdown
             print("Shutdown using exec");
-            exec('sudo shutdown -h now');
+            try {
+                exec('sudo shutdown -h now');
+                }
+            catch(Exception $e) {
+                print("Failed to shutdown with exec: <br>". $e->getMessage());
+            }
+
             //Execute Shutdown
             print("Shutdown using system");
-            system('sudo shutdown -h now');
+            try {
+                system('sudo shutdown -h now');
+                }
+            catch(Exception $e) {
+                print("Failed to shutdown with system: <br>". $e->getMessage());
+                }
         ?>
     </body>
 </html>
