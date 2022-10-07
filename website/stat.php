d25fdb89629117e5c6c18a9c0a8128ee5fecb0ff
inglele <inglele@live.com>
2022-10-07 15:46:44 -0700

    create stat page

--- website/stat.php
+++ website/stat.php
@@ -7,9 +7,47 @@
 // Drop down:
 // * Current week
 // * Current month
-// * Current last 3 months
+// * Last 3 months
 // * Current semester
-//
+
+
+// For debug, enable the following
+// ini_set("display_error", "stderr");
+// ini_set("display_startup_errors", 1);
+// ini_set("log_errors", 1);
+// ini_set("html_errors", 1);
+
+// DB Connection settings - mysql_variables.php
+//$db_host
+//$db_user
+//$db_pass
+//$db_name
+include_once 'mysql_variables.php';
+
+// Make connection to database
+$connectdb = mysqli_connect($db_host, $db_user, $db_pass) or die ("ERROR - Cannot reach database");
+mysqli_select_db($connectdb,$db_name) or die ("ERROR - Cannot select database");
+
+// TABLE buttondata(
+//	id INT PRIMARY KEY auto_increment,
+//	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//	category TEXT,
+//	state TEXT); 
+
+// show queries from the current date and the day before
+if(!isset($_POST['days'])){
+	$days = 7;
+}else{
+	$days = intval($_POST['days']);
+}
+if(isset($_POST['category']) && in_array($_POST['category'], ["pee", "poo", "fed"])){
+	$category = $_POST['category'];
+	$sql = "SELECT * FROM buttondata WHERE category = '$category' AND created >= CURRENT_DATE() - INTERVAL ".($days-1)." day ORDER BY id DESC;";
+}else{
+	$sql = "SELECT * FROM buttondata WHERE created >= CURRENT_DATE() - INTERVAL ".($days-1)." day ORDER BY id DESC;";
+}
+
+$results = mysqli_query($connectdb, $sql);
 ?>
 
 <html>
@@ -32,74 +70,36 @@ td{
 	font-size: 32px;
 	padding: 2px;
 }
-.pee{
-	background-color: #ffff66;
-}
-
-.poo{
-	background-color: #996600;
-}
-
-.fed{
-	background-color: #add8e6;
-}
 </style>
 </head>
 
 <body>
-<hr size=1>
-
-<table border="0" cellpadding="1" cellspacing="1" align="center">
-	<!-- add export link -->
-	<tr>
-		<th><a href='export_data.php'>Export all data from DB</a></th>
-		<th><a href='off.php' target='_blank'>Turn off Raspberry PI</a></th>
-	</tr>
-
-</table>
-
-<hr width='500px' size=1>
 
 <form method='POST'>
 <center>
 
 <?php
-if ($days > 1)
-    print "Baby's vital functions for the last $days days.";
-else
-    print "Baby's vital functions for the last $days day.";
+    print "Baby's stats for the last $days days.";
 ?>
 
-Show 
-<select name='category'>
-<option value='all'>All</option>
-<option value='pee'>Pee</option>
-<option value='poo'>Poop</option>
-<option value='fed'>Feeding</option>
+Show data for past <select name='days'>
+<option value='7'>week</option>
+<option value='30'>month</option>
+<option value='90'>3 months</option>
+<option value='180'>semester</option>
+<option value='365'>year</option>
 </select>
-events for past <select name='days'>
-<option value='1'>1</option>
-<option value='2'>2</option>
-<option value='3'>3</option>
-<option value='4'>4</option>
-<option value='5'>5</option>
-<option value='6'>6</option>
-<option value='7'>7</option>
-<option value='14'>14</option>
-<option value='21'>21</option>
-<option value='31'>31</option>
-<option value='365'>365</option>
-</select> days.
 <input type='submit' value='Update'>
 </center>
 </form>
 
-<table width="500px" border="1" cellpadding="1" cellspacing="1" align="center">
+<table border="1" cellpadding="1" cellspacing="1" align="center">
 <tr>
-<th width='50px'>ID</th>
-<th width='300px'>Date & Time</th>
-<th width='100px'>Category</th>
-<th width='50px'>State</th>
+<th>Date & Time</th>
+<th>&#128166; Count</th>
+<th>&#128169; Count</th>
+<th>&#x1f37c; Count</th>
+<th>&#x1f37c; Duration</th>
 <tr>
 <?php
 $event_count = 0;
@@ -107,31 +107,22 @@ $event_count = 0;
 while($event = mysqli_fetch_assoc($results)){
 	$event_count++;
 	echo "<tr>";
-	if ($event['category'] == "fed"){
-		echo "<td class='fed'><center>". $event['id'] ."</center></td>";
-		echo "<td class='fed'><center>". date("d M y G:i", strtotime($event['created'])) ."</center></td>";
-		echo "<td class='fed'><center>&#x1f37c;</center></td>"; //Show baby bottle emoji
-		echo "<td class='fed'><center>". $event['state'] ."</center></td>";
-	}else if ($event['category'] == "pee"){
-		echo "<td class='pee'><center>". $event['id'] ."</center></td>";
-		echo "<td class='pee'><center>". date("d M y G:i", strtotime($event['created'])) ."</center></td>";
-		echo "<td class='pee'><center>&#128166;</center></td>";  //Show pee emoji
-		echo "<td class='pee'><center>". $event['state'] ."</center></td>";
-	}else if ($event['category'] == "poo"){
-		echo "<td class='poo'><center>". $event['id'] ."</center></td>";
-		echo "<td class='poo'><center>". date("d M y G:i", strtotime($event['created'])) ."</center></td>";
-		echo "<td class='poo'><center>&#128169;</center></td>";  //Show poop emoji
-		echo "<td class='poo'><center>". $event['state'] ."</center></td>";
-	}else{
-		echo "<td><center>". $event['id'] ."</center></td>";
+	
+	try {
 		echo "<td><center>". date("d M y G:i", strtotime($event['created'])) ."</center></td>";
-		echo "<td style='background-color: red;'><center><b>Error</b></center></td>";
-		echo "<td><center>". $event['state'] ."</center></td>";
+		echo "<td class='pee'><center>". $event['pee_count'] ."</center></td>";
+		echo "<td class='poo'><center>". $event['poo_count'] ."</center></td>";
+		echo "<td class='fed'><center>". $event['fee_count'] ."</center></td>";
+		echo "<td class='fed'><center>". $event['fee_time'] ."</center></td>";
+		}
+	catch:
+		{
+		echo "<td><center>error</center></td>";
 	}
 	
-	echo "<tr>\n";
+	echo "</tr>";
 }
-echo "</table>\n";
+echo "</table>";
 echo "<p align='center'>Event count: $event_count<br></p>";
 
 ?>
