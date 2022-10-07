17009551b64a1a9a1909110e63b1288c9c0aac94
inglele <inglele@live.com>
2022-10-07 12:28:45 -0700

    updating log on shutdown

--- script/shutdown.py
+++ script/shutdown.py
@@ -4,6 +4,11 @@
 # =========================================================
 command = "/usr/bin/sudo /sbin/shutdown -h now"
 import subprocess
+
+# Executing shell
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 output = process.communicate()[0]
-print output
+
+# Show log 
+print ("Executing:", command)
+print ("Output of command:", output)
