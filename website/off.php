9d62eaf8a5b74d8fdafbc02f38e9acb12946065b
inglele <inglele@live.com>
2022-10-07 12:49:52 -0700

    new try catch for php execution

--- website/off.php
+++ website/off.php
@@ -3,24 +3,39 @@
 // =========================================================
 // https://www.pihome.eu/2017/10/11/enable-rebootshutdown-rpi-web/
 //
-
-print("<html><head><title>Baby Logger &#x1F476; - Turn off page</title></head><body>")
-
-//Execute Shutdown
-print("Shutdown using php");
-    
-// Try via PHP
-exec('/usr/bin/sudo /sbin/shutdown -h now', $output);
-print("Result:", $output);
-
-// Try via Python
-print("Shutdown using python script");
-try {
-    exec("python /var/www/shutdown.py");
-    }
-catch(Exception $e) {
-    print("Failed to shutdown with exec: <br>". $e->getMessage());
-}
-
-print("</body></html>")
 ?>
+<html>
+    <head>
+        <title>Baby Logger &#x1F476; - Turn off page</title>
+    </head>
+    <body>
+
+        <p>Shutdown using php</p>
+        <p>Result:</p>
+            <?php 
+            // Try via PHP
+            try {
+                exec('/usr/bin/sudo /sbin/shutdown -h now', $output);
+                print("Result:", $output);
+            }
+            catch(Exception $e) {
+                print("Failed to shutdown via PHP: <br>". $e->getMessage());
+            }
+        ?>
+
+        <p>Shutdown using python</p>
+        <p>Result:</p>
+    
+            <?php
+            // Try via Python
+            try {
+                exec("python3 /var/www/shutdown.py");
+                }
+            catch(Exception $e) {
+                print("Failed to shutdown with Python: <br>". $e->getMessage());
+            }
+
+        ?>
+
+    </body>
+</html>
