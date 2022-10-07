946d839b2322cf2ee274a9a0fb8c15d2b05222fe
inglele <inglele@live.com>
2022-10-07 12:28:14 -0700

    updated shutdown

--- website/ver.php
+++ website/ver.php
@@ -1,1 +1,12 @@
-<?php phpinfo(); ?>
+<?php 
+// PHP Test page
+// =========================================================
+
+// Save executed command
+$output = exec('ls -la');
+
+//Print the return value
+echo $output;
+
+phpinfo(); 
+?>
