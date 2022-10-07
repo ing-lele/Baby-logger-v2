22512a5941298794605690cf989fac4cf0a007b6
inglele <inglele@live.com>
2022-10-07 12:38:45 -0700

    update including all php

--- website/off.php
+++ website/off.php
@@ -3,29 +3,24 @@
 // =========================================================
 // https://www.pihome.eu/2017/10/11/enable-rebootshutdown-rpi-web/
 //
+
+print("<html><head><title>Baby Logger &#x1F476; - Turn off page</title></head><body>")
+
+//Execute Shutdown
+print("Shutdown using php");
+    
+// Try via PHP
+exec('/usr/bin/sudo /sbin/shutdown -h now', $output);
+print("Result:", $output);
+
+// Try via Python
+print("Shutdown using python script");
+try {
+    exec("python /var/www/shutdown.py");
+    }
+catch(Exception $e) {
+    print("Failed to shutdown with exec: <br>". $e->getMessage());
+}
+
+print("</body></html>")
 ?>
-
-<html>
-    <head>
-        <title>Baby Logger &#x1F476; - Turn off page</title>
-    </head>
-    <body>
-        <?php 
-            //Execute Shutdown
-            print("Shutdown using php");
-            
-            // Try via PHP
-            exec('/usr/bin/sudo /sbin/shutdown -h now', $output);
-            print("Result:", $output);
-
-            // Try via Python
-            print("Shutdown using python script");
-            try {
-                exec("python /var/www/shutdown.py");
-                }
-            catch(Exception $e) {
-                print("Failed to shutdown with exec: <br>". $e->getMessage());
-            }
-        ?>
-    </body>
-</html>
