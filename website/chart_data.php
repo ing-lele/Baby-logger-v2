2c0c6a442788eaa5594ffc26e60d6faff3fe04b2
inglele <inglele@live.com>
2022-10-09 18:39:09 -0700

    auto generic dataset and chart data

--- website/chart_data.php
+++ website/chart_data.php
@@ -79,77 +79,103 @@ while($event = mysqli_fetch_assoc($results)){
     $data_fed_duration[] = $event['fed_duration'];
 
 }
-echo "SQL raw result: <br>";
-print_r ($results);
 
-
-// Print to check ata structure and values
-echo "Lable array: <br>";
+// Print to check arrays values
+echo "<br>Lable array: ";
 print_r($x_labels);
 
-echo "Pee array: <br>";
+echo "<br>Pee array: ";
 print_r($data_pee_count);
 
-echo "Poo array: <br>";
+echo "<br>Poo array: ";
 print_r($data_poo_count);
 
-echo "Fed array: <br>";
+echo "<br>Fed array: ";
 print_r($data_fed_count);
 
-echo "Duration array: <br>";
+echo "<br>Duration array: ";
 print_r($data_fed_duration);
 
-/*
+
 // --------------------------
 // Initialize dataset
+
+// Chart -> Config -> Data -> Dataset #1 -> Pee count
 $datasets_pee_count = array (
     'type' => "line",
     'label' => "Pee Count",
     'backgroundColor' => "#ffff66",
     'borderColor' => "#ffff66",
-    'data' => $data_pee_count[]
+    'data' => $data_pee_count
 );
 
+// Chart -> Config -> Data -> Dataset #2 -> Poo count
 $datasets_poo_count = array(
     'type' => "line",
     'label' => "Poo Count",
     'backgroundColor' => "#996600",
     'borderColor' => "#996600",
-    'data' => $data_poo_count[]
+    'data' => $data_poo_count
 );
 
+// Chart -> Config -> Data -> Dataset #3 -> Milk count
 $datasets_fed_count = array(
-
+    'type' => "line",
+    'label' => "Milk Count",
+    'backgroundColor' => "#399cbd",
+    'borderColor' => "#399cbd",
+    'data' => $data_fed_count
 );
 
+// Chart -> Config -> Data -> Dataset #4 -> Milk duration
 $datasets_fed_duration = array(
-
+    'type' => "bar",
+    'label' => "Milk Duration",
+    'backgroundColor' => "#add8e6",
+    'borderColor' => "#add8e6",
+    'data' => $data_fed_duration
 );
 
+echo "<br>Pee count dataset: ";
+print_r($datasets_pee_count);
 
-$arrReturn = array(
+echo "<br>Poo count dataset: ";
+print_r($datasets_poo_count);
+
+echo "<br>Fed count dataset: ";
+print_r($datasets_fed_count);
+
+echo "<br>Fed duration dataset: ";
+print_r($datasets_fed_duration);
+
+// --------------------------
+// Initialize Chart Data
+//
+// Chart -> Config -> Data = 
+// {
+//      labels:
+//      dataset: [
+//          {type1, label1, data1},
+//          {type2, label2, data2},
+//          {...}
+//      ]
+// }
+$chart_data = array(
     array(
         'labels' => $x_labels,
-        'datasets' => $arrDatasets));
+        'datasets' => array(
+            $datasets_pee_count,
+            $datasets_poo_count,
+            $datasets_fed_count,
+            $datasets_fed_duration
+        )
+    )
+);
 
-print (json_encode($arrReturn));
+echo "<br>Chart data: ";
+print_r ($chart_data);
 
+// Encond in JSON format
+print(json_encode($chart_data));
 
-
-// --------------------------------
-
-
-
-// --------------------------------------------
-
-$i=0; $q=mysql_query('select ..');
-
-while($row=mysql_fetch_array($q)){               
-
-    echo "myarray[".$i."]='".$row['data']."';";
-
-    $i++;  
-}
-
-*/
 ?>
