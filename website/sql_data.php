b9f62cab882a97309d889d9e0e73c0892f95b836
inglele <inglele@live.com>
2022-10-11 19:45:38 -0700

    unix_timestamp on date + time_to_sec for duration

--- website/sql_data.php
+++ website/sql_data.php
@@ -38,11 +38,11 @@ function get_sql_data(int $weeks, string $sort) {
 
     // Show count of pee, poo, fed, fed_duration by day
     $sql_query = "SELECT 
-        DATE(ts_start) AS day, 
+        UNIX_TIMESTAMP(DATE(ts_start)) AS day, 
         COUNT(CASE WHEN category = 'pee' THEN id END) AS pee_count,
         COUNT(CASE WHEN category = 'poo' THEN id END) AS poo_count,
         COUNT(CASE WHEN category = 'fed' THEN id END) AS fed_count,
-        SEC_TO_TIME(SUM(CASE WHEN category = 'fed' THEN TIME_TO_SEC(TIMEDIFF(ts_end,ts_start)) END)) AS fed_duration 
+        SUM(CASE WHEN category = 'fed' THEN TIME_TO_SEC(TIMEDIFF(ts_end,ts_start)) END) AS fed_duration 
         FROM switchdata
         WHERE ts_start>= CURRENT_DATE() - INTERVAL ".($weeks)." WEEK 
         GROUP BY DATE(ts_start)
