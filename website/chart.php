8449f6495b02693c33f4f93d637d7a36f1eda371
inglele <inglele@live.com>
2022-10-11 20:07:17 -0700

    updated using gmdate without timezone

--- website/chart.php
+++ website/chart.php
@@ -123,14 +123,14 @@ foreach($sql_data as $event){
     //	pee_count INT,
     //	poo_count INT,
     //  fed_count INT,
-    //	fed_time TIME]
+    //	fed_time TIME_TO_SEC]
     
     try {
         $x_labels[] = date("d M", $event['day']);
         $data_pee_count[]  = $event['pee_count'];
         $data_poo_count[] = $event['poo_count'];
         $data_fed_count[] = $event['fed_count'];
-        $data_fed_duration[] = date("G:i:s", $event['fed_duration']);
+        $data_fed_duration[] = gmdate("G:i:s", $event['fed_duration']);
     }
     catch (Exception $ex) {
         echo "<td><center>Failed to create table</center></td>";
