f681ac1ad8154982e93a96dfb01628bc31d84fe6
inglele <inglele@live.com>
2022-10-18 19:04:36 -0700

    added gantt chart

--- website/navigation.php
+++ website/navigation.php
@@ -2,7 +2,8 @@
 	<tr>
 		<td><a href='/'>Log entry</a></td>
 		<td><a href='stat.php'>Statistics</a></td>
-		<td><a href='chart.php'>Charts</a></td>
+		<td><a href='chart.php'>Chart</a></td>
+		<td><a href='gantt.php'>Gantt</a></td>
 	</tr>
 </table>
 
