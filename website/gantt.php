0491a3070a17c5f9450d9d0ffe026199db50b1a9
inglele <inglele@live.com>
2022-10-19 17:35:52 -0700

    migrating from anychart 8.8.0 to 8.11.0

--- website/gantt.php
+++ website/gantt.php
@@ -1,83 +1,89 @@
-<?php
-// Generate gantt chart using Chart.js
-// Info: https://www.chartjs.org/
-// =========================================================
-
-// Read weeks from POST Form
-if(!isset($_POST['weeks'])) {
-    // Default to 2 weeks
-    $weeks = 2;
-    //echo "<p align='center'>Set default weeks: $weeks </p>\n";
-}
-else {
-    // Read from POST
-    $weeks = intval($_POST['weeks']);
-    //echo "<p align='center'>Weeks after POST: $weeks , is integer? ". is_int($weeks) ."</p>\n";
-}
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="width: 100%; height: 100%;">
 
 <head>
-    <title>Baby &#x1F476; Charts</title>
+    <title>Baby &#x1F476; Gantt</title>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="baby_logger.css">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js">
-        import 'chartjs-adapter-date-fns';
+    <script src="https://cdn.anychart.com/releases/8.8.0/js/anychart-bundle.min.js"></script>
+    
+    <script>
+        anychart.onDocumentReady(function () {
+            // The data used in this sample can be obtained from the CDN
+            // https://cdn.anychart.com/samples-data/gantt-charts/server-status-list/data.json
+            anychart.data.loadJsonFile('gantt_data.json', function (data) {
+                // create data tree on our data
+                var treeData = anychart.data.tree(data, 'as-table');
+
+                // create project gantt chart
+                var chart = anychart.ganttResource();
+
+                // set data for the chart
+                chart.data(treeData);
+
+                // set start splitter position settings
+                chart.splitterPosition(280);
+
+                // get chart data grid link to set column settings
+                var dataGrid = chart.dataGrid();
+
+                dataGrid.column(0).enabled(false);
+
+                // set first column settings
+                var firstColumn = dataGrid.column(1);
+                firstColumn.cellTextSettings().hAlign('left');
+                firstColumn.title('Duration')
+                    .width(150)
+                    .cellTextSettingsOverrider(labelTextSettingsOverrider)
+                    .format(function (item) {
+                        return item.get('name');
+                    });
+
+                // set first column settings
+                var secondColumn = dataGrid.column(2);
+                secondColumn.cellTextSettings().hAlign('right');
+                secondColumn.title('Free')
+                    .width(60)
+                    .cellTextSettingsOverrider(labelTextSettingsOverrider)
+                    .format(function (item) {
+                        return item.get('free') || '';
+                    });
+
+                // set first column settings
+                var thirdColumn = dataGrid.column(3);
+                thirdColumn.cellTextSettings().hAlign('right');
+                thirdColumn.title('Milk')
+                    .width(60)
+                    .cellTextSettingsOverrider(labelTextSettingsOverrider)
+                    .format(function (item) {
+                        return item.get('milk') || '';
+                    });
+
+                // set container id for the chart
+                chart.container('BabyStatChart');
+
+                chart.draw();
+
+                chart.zoomTo(Date.UTC(2008, 0, 31, 1, 36), Date.UTC(2008, 1, 15, 10, 3));
+
+            });
+        });
+
+        function labelTextSettingsOverrider(label, item) {
+            switch (item.get('type')) {
+                case 'free':
+                    label.fontColor('black').fontWeight('bold');
+                    break;
+                case 'milk':
+                    label.fontColor('blue').fontWeight('bold');
+                    break;
+            }
+        }
     </script>
 </head>
 
-<body>
-
-<?php
-	// Include Navigation bar
-	include_once 'navigation.php';
-?>
-
-<center>
-    <form method='POST' action=<?php echo $_SERVER['PHP_SELF'];?>>
-        <?php echo "<P>Baby's stats for last <b>$weeks weeks</b> since ". date("d M Y", strtotime('-'.$weeks.' weeks')) .".</P>"; ?>
-
-		Show stats for past <select name='weeks' id='weeks'>
-		<option value=2 <?php ($weeks==2) ? print('selected') : ''; ?>>2</option>
-		<option value=4 <?php ($weeks==4) ? print('selected') : ''; ?>>4</option> 
-		<option value=9 <?php ($weeks==9) ? print('selected') : ''; ?>>9</option>
-		<option value=13 <?php ($weeks==13) ? print('selected') : ''; ?>>13</option>
-		<option value=26 <?php ($weeks==26) ? print('selected') : ''; ?>>26</option>
-		<option value=52 <?php ($weeks==52) ? print('selected') : ''; ?>>52</option>
-		</select> weeks.
-        <input type='submit' value='Update'>
-    </form>
-</center>
-
-<!-- chart.js to create chart
-https://www.chartjs.org/docs/latest/getting-started/
-
-Creating canvas -->
-<div style="text-align: center">
-    <div class="wrapper">
-        <canvas id='BabyStatChart'></canvas>
-    </div>
-</div>
-
-<script>
-
-// Get Data
-chart_config = <?php require 'gantt_data.php' ?>;
-
-// Get context
-ctx = document.getElementById("BabyStatChart").getContext("2d");
-
-// Draw chart
-var MyBabyStatChart = new Chart(
-    ctx,
-    chart_config
-);
-
-</script>
-
+<body style="width: 90%; height: 90%; text-align: center;">
+    <div id="BabyStatChart"></div>
 </body>
+
 </html>
